56c3a5f8337edb69b9ac47f2131603451b479348
franziska.dudichum <franzi-du@web.de>
2023-06-30 23:59:27 +0200

    upload full webapp

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # P4_SS23 StepScore Dudichum
- This is the prototype of my semesterproject StepScore
+
+This is the prototype of my semesterproject "StepScore"
+P4 SS23 Franziska Dudichum Team 5
+
+<h2>How to run the code</h2>
+
+1. npm install
+
+2. npm start
+
+3. Finished!
+
+<h2>used packages:</h2>
+
+create-react-app
+bootstrap 5.3
---
+++ stepscore/src/components/Main/Main.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import "./Main.css";
+import logo from "../../assets/bechtle-logo.png";
+import Bild1 from "../../assets/protein.png";
+import Bild2 from "../../assets/apple-watch.jpg";
+import sorry from "../../assets/sorry.png";
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [showComponentA, setShowComponentA] = useState(false);
+  const [showComponentB, setShowComponentB] = useState(false);
+  const [showNoPrice, setShowNoPrice] = useState(false);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (inputValue === "10000") {
+      setShowComponentA(true);
+      setShowComponentB(false);
+      setShowNoPrice(false);
+    } else if (inputValue === "20000") {
+      setShowComponentA(false);
+      setShowComponentB(true);
+      setShowNoPrice(false);
+    } else {
+      setShowComponentA(false);
+      setShowComponentB(false);
+      setShowNoPrice(true);
+    }
+
+    setInputValue("");
+  };
+
+  return (
+    <div className="Main">
+      <img src={logo} className="logo" alt="..." />
+      <h1>StepScore</h1>
+      <p className="text-1">a semesterproject by Franziska Dudichum</p>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="gelaufene Schritte eingeben"
+            aria-label="countedSteps"
+            aria-describedby="button-addon2"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button
+            className="btn btn-outline-secondary"
+            type="Senden"
+            id="button-addon2"
+          >
+            Button
+          </button>
+        </div>
+      </form>
+      {showComponentA && <ComponentA />}
+      {showComponentB && <ComponentB />}
+      {showNoPrice && <NoPrice />}
+    </div>
+  );
+};
+
+const NoPrice = () => {
+  return (
+    <div className="card">
+      <img src={sorry} className="card-Bild" alt="..."></img>
+      <div className="card-body">
+        <h5 className="card-title">Tut uns Leid!</h5>
+        <p className="card-text">
+          Deine Schritte reichen leider noch nicht für eine Prämie aus.
+        </p>
+        <p>Versuche es später noch einmal.</p>
+      </div>
+    </div>
+  );
+};
+
+const ComponentA = () => {
+  return (
+    <div className="card">
+      <img src={Bild1} className="card-Bild" alt="..."></img>
+      <div className="card-body">
+        <h5 className="card-title">Prämie 1</h5>
+        <p className="card-text">Herzlichen Glückwunsch! Deine Prämie:</p>
+        <p>eine Protein Riegel!</p>
+        <a
+          href="https://www.bechtle.com/ueber-bechtle/presse/pressemeldungen/2022/bechtle-erhaelt-auszeichnung-gesunder-arbeitgeber"
+          className="btn btn-primary"
+        >
+          Einlösen
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const ComponentB = () => {
+  return (
+    <div className="card">
+      <img src={Bild2} className="card-Bild" alt="..."></img>
+      <div className="card-body">
+        <h5 className="card-title">Prämie 2</h5>
+        <p className="card-text">Herzlichen Glückwunsch! Deine Prämie:</p>
+        <p>eine Apple Watch!</p>
+
+        <a
+          href="https://www.bechtle.com/ueber-bechtle/presse/pressemeldungen/2022/bechtle-erhaelt-auszeichnung-gesunder-arbeitgeber"
+          className="btn btn-primary"
+        >
+          Einlösen
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
